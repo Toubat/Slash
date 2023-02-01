80ab2115cc40786c2e0387bc47c7924724469535
Boquan (Brian) Yin <57741529+Toubat@users.noreply.github.com>
2023-02-01 01:41:05 -0600

    update

--- Source/Slash/Private/Items/Item.cpp
+++ Source/Slash/Private/Items/Item.cpp
@@ -3,6 +3,7 @@
 
 #include "Items/Item.h"
 #include "Slash/DebugMacros.h"
+#include "Components/SphereComponent.h"
 
 AItem::AItem()
 {
@@ -11,30 +12,32 @@ AItem::AItem()
 	// Create the mesh component
 	ItemMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ItemMeshComponent"));
 	RootComponent = ItemMesh;
+
+	Sphere = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComponent"));
+	Sphere->SetupAttachment(GetRootComponent());
 }
 
 void AItem::BeginPlay()
 {
 	Super::BeginPlay();
 
+	Sphere->OnComponentBeginOverlap.AddDynamic(this, &AItem::OnSphereBeginOverlap);
+	Sphere->OnComponentEndOverlap.AddDynamic(this, &AItem::OnSphereEndOverlap);
 }
 
-float AItem::TransformSin()
+void AItem::OnSphereBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	return Amplitude * FMath::Sin(Frequency * RunningTime);
+	const auto ActiorName = OtherActor->GetName();
+
+	if (GEngine) GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, ActiorName);
 }
 
-float AItem::TransformCos()
+void AItem::OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
 {
-	return Amplitude * FMath::Cos(Frequency * RunningTime);
-}
+	const auto ActiorName = FString("Ending Overlap with: ") + OtherActor->GetName();
 
-template<typename T>
-inline T AItem::Avg(T first, T second)
-{
-	return (first + second) / 2;
+	if (GEngine) GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, ActiorName);
 }
-
 
 void AItem::Tick(float DeltaTime)
 {
@@ -45,4 +48,20 @@ void AItem::Tick(float DeltaTime)
 	FRotator NewRotation = GetActorRotation();
 	NewRotation.Yaw += 100.f * DeltaTime;
 	SetActorRotation(NewRotation);
+}
+
+template<typename T>
+inline T AItem::Avg(T first, T second)
+{
+	return (first + second) / 2;
+}
+
+float AItem::TransformSin()
+{
+	return Amplitude * FMath::Sin(Frequency * RunningTime);
+}
+
+float AItem::TransformCos()
+{
+	return Amplitude * FMath::Cos(Frequency * RunningTime);
 }
--- Source/Slash/Public/Items/Item.h
+++ Source/Slash/Public/Items/Item.h
@@ -6,6 +6,8 @@
 #include "GameFramework/Actor.h"
 #include "Item.generated.h"
 
+class USphereComponent;
+
 UCLASS()
 class SLASH_API AItem : public AActor
 {
@@ -34,11 +36,20 @@ protected:
 	template<typename T>
 	T Avg(T first, T second);
 
+	UFUNCTION()
+	void OnSphereBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+	
 private:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
 	float RunningTime;
 
 	UPROPERTY(VisibleAnywhere)
 	UStaticMeshComponent* ItemMesh;
+
+	UPROPERTY(VisibleAnywhere)
+	USphereComponent* Sphere;
 };
 
